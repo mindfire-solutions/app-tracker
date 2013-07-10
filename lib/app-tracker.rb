12610eaadf06f03ae7093b0d933d81c1431d9a70
Sahil Grover <gr.sahil20@gmail.com>
2013-07-10 15:15:23 +0530

    added helper method active_class in module

--- lib/app-tracker.rb
+++ lib/app-tracker.rb
@@ -1,8 +1,14 @@
 require "app-tracker/engine"
 
+module ApplicationHelper
+	def active_class(attr, tab)
+		"active" if attr == tab
+	end
+end
+
 module AppTracker
 	def request_tracker
-   		user_agent = UserAgent.parse(request.user_agent)
+		user_agent = UserAgent.parse(request.user_agent)
 		request_type = formatted_format(request.format)
 
 		os = user_agent.platform.nil? ? ('unknown'):(user_agent.platform)
@@ -10,7 +16,7 @@ module AppTracker
 		country = Timeout::timeout(10) { Net::HTTP.get_response(URI.parse('http://api.hostip.info/country.php?ip=' + request.remote_ip )).body } rescue "XX"
 		country = 'unknown' if country == 'XX'
 
-    	AppTracker::Log.create!(
+		AppTracker::Log.create!(
 			:os => os,
 			:browser => browser,
 			:controller => params[:controller],
@@ -21,18 +27,18 @@ module AppTracker
 			:previous_url => request.referrer,
 			:current_url => "http://#{request.host}:#{request.port.to_s + request.fullpath}"
 			#:created_at => Date.today
-    	)
+			)
 	end
 
 	def formatted_format(request_type)
-	 case request_type
+		case request_type
 		when 'text/html' || 'html'
 			'html'
- 		when 'text/javascript' || 'javascript'
+		when 'text/javascript' || 'javascript'
 			'javascript'
- 		when 'application/json' || 'json'
+		when 'application/json' || 'json'
 			'json'
- 		when 'application/xml' || 'xml'
+		when 'application/xml' || 'xml'
 			'xml'
 		else
 			'unknown'
